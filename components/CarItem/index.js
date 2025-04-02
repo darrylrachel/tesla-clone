ddb318297503e26466dfec6878adac97668206c4
darrylrachel <racheldarryl@icloud.com>
2025-04-02 13:44:27 -0700

    Add and styled buttons

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
+import StyledButton from '../StyledButton';
 import styles from './styles';
 
 const carItem = () => {
@@ -10,6 +11,22 @@ const carItem = () => {
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
+
+      <StyledButton 
+        type='primary' 
+        content={'Custom Order'} 
+        onPress={() => {
+          console.warn('Custom Order was pressed')
+        }} 
+      />
+
+      <StyledButton 
+        type='secondary' 
+        content={'Existing Inventory'} 
+        onPress={() => {
+          console.warn('Existing Inventory was pressed')
+        }} 
+      />
     </View>
   );
 };
